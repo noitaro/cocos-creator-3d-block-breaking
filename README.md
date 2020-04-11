62d7f74cd6cb4d05d24a55cd72a816c00ad29e88
noitaro <wk.logonasi.zwolf@gmail.com>
2020-04-11 22:21:04 +0900

    Bar move

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # cocos-creator-3d-block-breaking
-![](cocos-creator-3d-block-breaking.png)
+![](cocos-creator-3d-block-breaking-1.png)
 ## WebSite
 https://noitalog.tokyo/cocos-creator-3d-block-breaking-1/
--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -1,16 +1,49 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Helloworld extends cc.Component {
+  @property(cc.Node)
+  Player: cc.Node = null;
 
-    @property(cc.Label)
-    label: cc.Label = null;
+  AccLeft: Boolean = false;
+  AccRight: Boolean = false;
 
-    @property
-    text: string = 'hello';
+  start() {
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+  }
 
-    start () {
-        // init logic
-        this.label.string = this.text;
+  update() {
+    if (this.AccLeft) {
+      this.Player.x -= 8;
+    } else if (this.AccRight) {
+      this.Player.x += 8;
     }
+  }
+
+  onKeyDown(event: cc.Event.EventKeyboard) {
+    switch (event.keyCode) {
+      case cc.macro.KEY.left:
+        this.AccLeft = true;
+        break;
+      case cc.macro.KEY.right:
+        this.AccRight = true;
+        break;
+      default:
+        break;
+    }
+  }
+
+  onKeyUp(event: cc.Event.EventKeyboard) {
+    switch (event.keyCode) {
+      case cc.macro.KEY.left:
+        this.AccLeft = false;
+        break;
+      case cc.macro.KEY.right:
+        this.AccRight = false;
+        break;
+      default:
+        break;
+    }
+  }
 }
